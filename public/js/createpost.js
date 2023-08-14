6438d4ca5a352e2c9d5ac222b546715207c07809
Derek Meduri <derekmeduri@gmail.com>
2023-08-13 20:07:58 -0400

    adding create post js

---
+++ public/js/createpost.js
@@ -0,0 +1,24 @@
+async function newFormHandler(event) {
+  event.preventDefault();
+
+  const title = document.querySelector().value;
+  const post_content = document.querySelector().value;
+
+  const response = await fetch("/apie/posts", {
+    method: "post",
+    body: JSON.stringify({
+      title,
+      post_content,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText);
+  }
+}
+//add event listener for submit button on creating new post then run function
+document.querySelector().addEventListener("submit", newFormHandler);
