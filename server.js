bc3c8e962dc8ce3a744801e5befa092944907a8a
Derek Meduri <derekmeduri@gmail.com>
2023-08-23 14:41:38 -0400

    cleaning up server

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const helpers = require("./utils/helpers");
-const exp = require("constants");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -44,6 +43,6 @@ app.use(routes);
 //connect to db before starting server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () =>
-    console.log("Now Listening on port 'http://localhost:${PORT}'")
+    console.log(`Now Listening on port 'http://localhost:${PORT}'`)
   );
 });
