54276cc40d1f417ad65a715e6ea3625681a3261f
Derek Meduri <derekmeduri@gmail.com>
2023-10-25 13:18:07 -0400

    fixing comment routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,55 +2,34 @@ const router = require("express").Router();
 const { Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-//get comments
-router.get("/", async (req, res) => {
+router.post("/posts", withAuth, async (req, res) => {
   try {
-    const commentData = await Comment.findAll({});
-    //turn comments into json object
-    res.json(commentData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-//get comment ids
-router.get("/:id", async (req, res) => {
-  try {
-    const commentData = await Comment.findAll({
-      where: { id: req.params.id },
-    });
-    res.json(commentData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-//post comment if user is logged in
-router.post("/", withAuth, async (req, res) => {
-  try {
-    const commmentData = await Comment.create({
-      comment_content: req.body.comment_content,
-      post_id: req.body.post_id,
+    const newComment = await Comment.create({
+      ...req.body,
       user_id: req.session.user_id,
     });
-    res.json(commmentData);
+
+    res.status(200).json(newComment);
   } catch (err) {
+    console.log(err);
     res.status(400).json(err);
   }
 });
 
-//delete comment if user is logged in
 router.delete("/:id", withAuth, async (req, res) => {
   try {
     const commentData = await Comment.destroy({
-      where: { id: req.params.id },
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
     });
+
     if (!commentData) {
-      res
-        .status(404)
-        .json({ message: "Cannot delete comment or no comment found." });
+      res.status(404).json({ message: "No comment found" });
       return;
     }
+    res.status(200).json(commentData);
   } catch (err) {
     res.status(500).json(err);
   }
